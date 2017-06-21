dfe8b4ef7a9d3f6a6be74a0205a673a45e937d8c
deigo1995 <diego1995>
2017-06-21 09:14:22 -0400

    Creacion de Plantillas

--- WebEscuela/WebEscuela/Plantillas/plantillaLogin.Master.designer.cs
+++ WebEscuela/WebEscuela/Plantillas/plantillaLogin.Master.designer.cs
@@ -7,11 +7,11 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WebEscuela
+namespace WebEscuela.Plantillas
 {
 
 
-    public partial class Site1
+    public partial class plantillaLogin
     {
 
         /// <summary>
