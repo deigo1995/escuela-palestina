2d4bc68840e9a174ab334f0fcde2392327abfed9
deigo1995 <diego1995>
2017-07-04 09:49:49 -0400

    Conexion

---
+++ WebEscuela/WebEscuela/Clases/Datos.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebEscuela.Clases
+{
+    public class Datos
+    {
+        private string cadena = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\EscuelaDB.mdf;Integrated Security=True";
+        public SqlConnection cn;
+        public SqlDataAdapter da;
+        public DataSet ds = new DataSet();
+        public SqlCommand comando;
+
+        private void Conectar()
+        {
+            cn = new SqlConnection(cadena);
+        }
+
+        public Datos()
+        {
+            Conectar();
+        }
+
+        public bool insertar(string sql)
+        {
+            cn.Open();
+            comando = new SqlCommand(sql, cn);
+            int i = comando.ExecuteNonQuery();
+            cn.Close();
+
+            if (i > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public bool modificar(string sql)
+        {
+            cn.Open();
+            comando = new SqlCommand(sql, cn);
+            int i = comando.ExecuteNonQuery();
+            cn.Close();
+
+            if (i > 0)
+            {
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public bool eliminar(string sql)
+        {
+            cn.Open();
+            comando = new SqlCommand(sql, cn);
+            int i = comando.ExecuteNonQuery();
+            cn.Close();
+
+            if (i > 0)
+            {
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
